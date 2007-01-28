cdc668fe59701f31f827f3f3767a70fce443be5c
Jim Garlick <garlick.jim@gmail.com>
2007-01-28 17:34:29 +0000

    handle >2tb device in linux

--- getsize.c
+++ getsize.c
@@ -55,11 +55,14 @@ extern char *prog;
 /* scrub-1.8 tested Fedora Core 5 */
 #include <sys/ioctl.h>
 #include <linux/fs.h>
+#include <sys/utsname.h>
 
 off_t 
 getsize(char *path)
 {
-    unsigned long numblocks;
+    struct utsname ut;
+    unsigned long long numbytes;
+    int valid_blkgetsize64 = 1;
     int fd;
 
     fd = open(path, O_RDONLY);
@@ -68,14 +71,32 @@ getsize(char *path)
         perror(path);
         exit(1);
     }
-    if (ioctl(fd, BLKGETSIZE, &numblocks) < 0) {
-        fprintf(stderr, "%s: ioctl BLKGETSIZE ", prog);
-        perror(path);
-        exit(1);
+
+    /* Ref: e2fsprogs-1.39 - apparently BLKGETSIZE64 doesn't work pre 2.6 */
+    if ((uname(&ut) == 0) &&
+            ((ut.release[0] == '2') && (ut.release[1] == '.') &&
+             (ut.release[2] < '6') && (ut.release[3] == '.')))
+                valid_blkgetsize64 = 0;
+    if (valid_blkgetsize64) {
+        if (ioctl(fd, BLKGETSIZE64, &numbytes) < 0) {
+            fprintf(stderr, "%s: ioctl BLKGETSIZE64 ", prog);
+            perror(path);
+            exit(1);
+        }
+    } else {
+        unsigned long numblocks;
+
+        if (ioctl(fd, BLKGETSIZE, &numblocks) < 0) {
+            fprintf(stderr, "%s: ioctl BLKGETSIZE ", prog);
+            perror(path);
+            exit(1);
+        }
+        numbytes = (off_t)numblocks*512; /* 2TB limit here */
     }
+
     (void)close(fd);
 
-    return (off_t)numblocks*512;
+    return (off_t)numbytes;
 }
 
 #elif defined(__FreeBSD__)
