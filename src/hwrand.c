3431726747857dba6f6ff2900d6d4d8f2bd1c59f
Jim Garlick <garlick.jim@gmail.com>
2012-04-16 16:00:32 -0700

    Update project URL in boilerlpate

--- src/hwrand.c
+++ src/hwrand.c
@@ -5,7 +5,7 @@
  *  Written by H. Peter Anvin <h.peter.anvin@intel.com>
  *
  *  This file is part of Scrub, a program for erasing disks.
- *  For details, see <http://www.llnl.gov/linux/scrub/>.
+ *  For details, see https://code.google.com/p/diskscrub/
  *
  *  Scrub is free software; you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free
